48b6aeb35af3f81968314233907ebf0feee56945
ha5him97 <avhashim7@gmail.com>
2024-11-11 23:23:08 +0530

    initial change

---
+++ top_players/script.js
@@ -0,0 +1,5 @@
+// JavaScript to toggle the "active" class on the playerInfo div
+document.getElementById("headerToggleButton").addEventListener("click", function() {
+    document.getElementById("playerInfo").classList.toggle("active");
+  });
+  
